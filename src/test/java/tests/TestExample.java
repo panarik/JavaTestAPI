fc44804c4b7cd1d0df6d89bd65e71773f6fb0a02
panarik <panarik@yandex.ru>
2021-09-01 19:04:13 +0300

    add first test

---
+++ src/test/java/tests/TestExample.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestExample {
+
+    @Test
+    public void test1() {
+        System.out.println("testing...");
+        Assert.assertTrue(true);
+    }
+
+}
